7ee26b88113d54d36387832dba78b5217df679fa
Marcel Arrufat <marrufat@zinio.com>
2016-05-27 17:35:51 +0200

    Adding solutions

--- week 1/recfun/src/main/scala/recfun/Main.scala
+++ week 1/recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,46 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+
+    if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+
+    def loop(charList: List[Char], open: Int): Boolean = {
+      if (charList.isEmpty) open == 0
+      else if (charList.head == '(') loop(charList.tail, open + 1)
+      else if (charList.head == ')' && open > 0) loop(charList.tail, open - 1)
+      else if (charList.head == ')' && open == 0) false
+      else loop(charList.tail, open)
+
+    }
+
+    loop(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def loop(moneyLoop: Int, coinsLoop: List[Int], count: Int): Int = {
+
+      if (moneyLoop == 0) 1
+      else if (moneyLoop < 0) 0
+      else if (coinsLoop.isEmpty) 0
+      else loop(moneyLoop - coinsLoop.head, coinsLoop, count) + loop(moneyLoop,
+        coinsLoop.tail, count)
+    }
+
+    loop(money, coins, 0)
+  }
+}
