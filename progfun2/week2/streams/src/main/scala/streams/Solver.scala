c44853993f33d97bf2e8253c4fb4421c5619295a
Marcel Arrufat <marrufat@zinio.com>
2016-07-17 22:51:00 +0200

    Final solution

--- progfun2/week2/streams/src/main/scala/streams/Solver.scala
+++ progfun2/week2/streams/src/main/scala/streams/Solver.scala
@@ -68,20 +68,36 @@ trait Solver extends GameDef {
   def from(initial: Stream[(Block, List[Move])],
            explored: Set[Block]): Stream[(Block, List[Move])] = {
 
-    newNeighborsOnly(neighborsWithHistory(initial.head._1, initial.head._2), explored)
+    if (initial isEmpty) {
+      println("initial was empty")
+      Stream.empty
+    }
+    else {
+      val more = for {
+        path <- initial
+        next <- newNeighborsOnly(neighborsWithHistory(path._1, path._2), explored)
+        if !(explored contains next._1)
+      } yield next
+      initial.head #:: from(initial.tail #::: more, explored ++ (more map (_._1)))
+    }
+
 
   }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = from
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List())), Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+    for {
+      path <- pathsFromStart
+      if (done(path._1))
+    } yield path
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -91,5 +107,5 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = (pathsToGoal.head)._2.reverse
 }
