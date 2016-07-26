cd29970526f18cf22cba243cbfd0e7f99f7f789b
Marcel Arrufat <marrufat@zinio.com>
2016-07-26 16:57:34 +0200

    Final submission

--- progfun2/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,7 +1,6 @@
 package quickcheck
 
 import org.scalacheck.Arbitrary.{apply => _, _}
-import org.scalacheck.Gen._
 import org.scalacheck.Prop._
 import org.scalacheck._
 
@@ -10,9 +9,8 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   lazy val genHeap: Gen[H] = for {
     n <- arbitrary[A]
-    m <- empty
-  } yield insert(n, m)
-  
+  } yield insert(n, empty)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -29,11 +27,60 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   }
 
-
   property("findMinOfBoth") = {
 
   val v1 = 2
   val v2 = 1
   findMin(insert(v2, insert(v1, empty))) == v2
+  }
+
+  property("insert + del = isEmpty") = {
+
+
+    isEmpty(deleteMin(insert(2, empty)))
+
+  }
+
+  property("Delete in empty") = {
+
+    val r = try {
+      deleteMin(empty)
+      false
+    } catch {
+      case e: NoSuchElementException =>
+        true
+    }
+
+    r
+  }
+
+  property("Meld two emtpy") = {
+
+    isEmpty(meld(empty, empty))
+  }
+
+  def checkOrder(h: H, last: Int, b: Boolean): Boolean = {
+
+    if (isEmpty(h)) b
+    else {
+      val currentMin = findMin(h)
+      checkOrder(deleteMin(h), currentMin, currentMin > last & b)
+    }
+
+  }
+
+  property("Sorted mins") = {
+
+    val h = insert(5, insert(3, insert(-2, empty)))
+
+    checkOrder(deleteMin(h), findMin(h), true)
+  }
+
+  property("Meld nil") = {
+
+    val h = insert(5, insert(3, empty))
+    val h2 = insert(-2, empty)
+
+    findMin(meld(h, h2))== -2
   }
 }
