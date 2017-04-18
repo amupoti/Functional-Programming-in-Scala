b9544a739ae50602b07be32c6f92cf1417b360f2
Marcel Arrufat <marrufat@zinio.com>
2017-04-18 14:14:55 +0200

    Initial commit for week 4

---
+++ scala-spark-big-data/week4/timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -0,0 +1,22 @@
+package timeusage
+
+import org.apache.spark.sql.{ColumnName, DataFrame, Row}
+import org.apache.spark.sql.types.{
+DoubleType,
+StringType,
+StructField,
+StructType
+}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.util.Random
+
+@RunWith(classOf[JUnitRunner])
+class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+  test("dfSchema returns a StructType with one string and the rest with doubles"){
+
+  }
+}
