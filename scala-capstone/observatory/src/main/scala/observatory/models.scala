55323bda95175402894cfc5641ecb0ca39ecd71f
Marcel Arrufat <marrufat@zinio.com>
2017-04-28 15:08:52 +0200

    adn according model update

--- scala-capstone/observatory/src/main/scala/observatory/models.scala
+++ scala-capstone/observatory/src/main/scala/observatory/models.scala
@@ -4,7 +4,8 @@ case class Location(lat: Double, lon: Double)
 
 case class Color(red: Int, green: Int, blue: Int)
 
-case class Station(id: String, lat: Double, lon: Double)
+case class Station(id: StationId, lat: Double, lon: Double)
 
-case class Temperature(stationId: String, month: Int, day: Int, temperature: Double)
+case class Temperature(stationId: StationId, month: Int, day: Int, temperature: Double)
 
+case class StationId(stn:String, wban:String)
