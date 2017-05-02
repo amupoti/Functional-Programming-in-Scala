cddcca7e9d8b63bfd18dbad3e1822529371c8144
Marcel Arrufat <marrufat@zinio.com>
2017-05-02 18:01:24 +0200

    First commit for week 2

--- scala-capstone/observatory/src/main/scala/observatory/Visualization.scala
+++ scala-capstone/observatory/src/main/scala/observatory/Visualization.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.lang.Math._
+
 import com.sksamuel.scrimage.{Image, Pixel}
 
 /**
@@ -7,27 +9,65 @@ import com.sksamuel.scrimage.{Image, Pixel}
   */
 object Visualization {
 
-  /**
-    * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
-    * @return The predicted temperature at `location`
-    */
-  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+  val spark = Extraction.spark
+  val earthRadius = 6371
+
+  def computeDistance(l1: Location, l2: Location) = {
+    //use great-circle distance
+    val ds = acos(sin(l1.lat) * sin(l2.lat) + cos(l1.lat) * cos(l2.lat) * cos(abs(l2.lon - l1.lon)))
+    ds * earthRadius
   }
 
   /**
+    * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
+    * @param location     Location where to predict the temperature
+    * @return The predicted temperature at `location`
+    */
+  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
+    //use Inverse distance weighting
+
+    val tempRdd = spark.sparkContext.parallelize(temperatures.toSeq)
+    val p = 2
+    val numDem = tempRdd.map(u => (
+      u._2 / pow(computeDistance(u._1, location), p),
+      (1 / pow(computeDistance(u._1, location), 2))))
+    numDem.map(_._1).sum() / numDem.map(_._2).sum()
+  }
+
+  def linearInterpolation(x0: (Double, Color), x1: (Double, Color), value: Double): Color = {
+
+    val percentage = value - x0._1 / x1._1 - x0._1
+    val diff = (x0._2.red - x1._2.red) * percentage
+    //TODO: complete method
+
+  }
+
+
+  /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    // use linear interpolation
+    val pointMap = points.toMap
+    val valueColor = pointMap.get(value)
+    val pointsSortedByTemp = points.toArray.sortBy(_._1)
+
+    //TODO: edge cases
+    val index = (1 until pointsSortedByTemp.size).
+      find(i => value < pointsSortedByTemp(i + 1)._1 && value > pointsSortedByTemp(i)._1).headOption
+    if (index.isEmpty) pointsSortedByTemp.last._2
+    else {
+      val i = index.get
+      linearInterpolation(pointsSortedByTemp(i - 1), pointsSortedByTemp(i), value)
+    }
+
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
--- scala-capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
+++ scala-capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -9,5 +9,32 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+  val points = Seq((60.0, Color(255, 255, 255)), (32.0, Color(255, 0, 0)), (12.0, Color(255, 255, 0)), (0.0,
+    Color(0, 255, 255)), (-15.0, Color(0, 0, 255)), (-27.0, Color(255, 0, 255)), (-50.0, Color(33, 0, 107))
+    , (-60.0, Color(0, 0, 0)))
+
+
+  test("Predicted temperature should be between provided known values") {
+    val t1 = (Location(0.0, 0.0), 10.0)
+    val t2 = (Location(2.0, 2.0), 15.0)
+    val predTemp = Visualization.predictTemperature(List(t1, t2), Location(1.0, 1.0))
+    println(s"Predicted temperature is $predTemp")
+    assert(predTemp > 10.0 && predTemp < 15.0)
+  }
+
+  test("interpolateColor must provide colors for well known boundaries") {
+    assert(Visualization.interpolateColor(points, 60.0) === Color(255, 255, 255))
+    assert(Visualization.interpolateColor(points, 32.0) === Color(255, 0, 0))
+    assert(Visualization.interpolateColor(points, 12.0) === Color(255, 255, 0))
+    assert(Visualization.interpolateColor(points, 0.0) === Color(0, 255, 255))
+    assert(Visualization.interpolateColor(points, -15.0) === Color(0, 0, 255))
+    assert(Visualization.interpolateColor(points, -27.0) === Color(255, 0, 255))
+    assert(Visualization.interpolateColor(points, -50.0) === Color(33, 0, 107))
+    assert(Visualization.interpolateColor(points, -60.0) === Color(0, 0, 0))
+  }
+  test("interpolateColor must provide colors for liniar interpolated values") {
+    val value = Visualization.interpolateColor(points, 59.0)
+    assert(value.green === 255)
+  }
 
 }
